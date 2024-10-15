fd0206c13fb8dbeb742be188ad0a96bf538ff72f
GLITCHxbot <sharukshamim21@gmail.com>
2024-10-15 09:53:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,11 +38,11 @@ def install_requirements():
 install_requirements()
 
 # 🎛️ Telegram API token (replace with your actual token)
-TOKEN = '7424479555:AAGtNAbqXax_b3b--QnGYi5_XwBbQNw'
+TOKEN = '7415345435:AAE64tGkMh5QCkjwfW1ldbY6OMTnoYj4yRw'
 bot = telebot.TeleBot(TOKEN, threaded=False)
 
 # 🛡️ List of authorized user IDs (replace with actual IDs)
-AUTHORIZED_USERS = [5588464519]
+AUTHORIZED_USERS = [6047633116]
 
 # 🌐 Global dictionary to keep track of user attacks
 user_attacks = {}
